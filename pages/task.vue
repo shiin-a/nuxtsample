0ff95e571537bdfefd5c8f6466daf124819ba249
Shiina <shiinayusuke01@gmail.com>
2024-08-10 16:45:10 +0900

    prismaから値を取得し、表示するページを追加

---
+++ pages/task.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data: tasks } = useFetch('/api/task');
+</script>
+<template>
+  <div>
+    <h1>Main Page</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task.task }}</li>
+    </ul>
+  </div>
+</template>
